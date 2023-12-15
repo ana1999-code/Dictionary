a8cff1a255fc47953f16283d32678d901e5b3f0f
Anisoara Vlasi <anisoara.vlasi@iongroup.com>
2023-12-15 16:23:49 +0200

    Disable security for WordControllerImplTest

--- src/test/java/com/example/dictionary/rest/controller/impl/WordControllerImplTest.java
+++ src/test/java/com/example/dictionary/rest/controller/impl/WordControllerImplTest.java
@@ -6,8 +6,10 @@ import com.example.dictionary.application.exception.ResourceNotFoundException;
 import com.example.dictionary.application.facade.WordFacade;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -39,6 +41,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(WordControllerImpl.class)
+@AutoConfigureMockMvc(addFilters = false)
 class WordControllerImplTest {
 
     public static final String URL = "/words";
@@ -50,6 +53,11 @@ class WordControllerImplTest {
     private WordFacade wordFacade;
 
     public static final ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeEach
+    void setUp() {
+        WORD_DTO.setName("test");
+    }
 
     @Test
     void testGetAllWords() throws Exception {
