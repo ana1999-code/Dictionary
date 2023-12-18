f9781dba8b2b983629b305dbd8f7397bc22f0ef1
Ana <anavlas1999@gmail.com>
2023-12-18 22:05:20 +0200

    Add tests for addDefinitionToWord and removeDefinitionFromWord from WordControllerImpl class

--- src/test/java/com/example/dictionary/rest/controller/impl/WordControllerImplTest.java
+++ src/test/java/com/example/dictionary/rest/controller/impl/WordControllerImplTest.java
@@ -1,7 +1,9 @@
 package com.example.dictionary.rest.controller.impl;
 
+import com.example.dictionary.application.dto.DefinitionDto;
 import com.example.dictionary.application.dto.WordDto;
 import com.example.dictionary.application.exception.DuplicateResourceException;
+import com.example.dictionary.application.exception.IllegalOperationException;
 import com.example.dictionary.application.exception.ResourceNotFoundException;
 import com.example.dictionary.application.facade.WordFacade;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -12,20 +14,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.example.dictionary.utils.TestUtils.DEFINITION_DTO;
+import static com.example.dictionary.utils.TestUtils.DEFINITION_IS_PRESENT;
+import static com.example.dictionary.utils.TestUtils.DEFINITION_NOT_FOUND;
+import static com.example.dictionary.utils.TestUtils.DEFINITION_NOT_FOUND_FOR_THE_WORD;
 import static com.example.dictionary.utils.TestUtils.DEFINITION_NOT_FOUND_FOR_WORD;
 import static com.example.dictionary.utils.TestUtils.DUPLICATE_WORD;
 import static com.example.dictionary.utils.TestUtils.EXAMPLE_NOT_CONTAINS_WORD;
+import static com.example.dictionary.utils.TestUtils.EXISTING_DEFINITION_DTO_FOR_WORD;
 import static com.example.dictionary.utils.TestUtils.INVALID_WORD;
-import static com.example.dictionary.utils.TestUtils.WORD;
+import static com.example.dictionary.utils.TestUtils.ONLY_ONE_DEFINITION;
 import static com.example.dictionary.utils.TestUtils.WORD_DTO;
 import static com.example.dictionary.utils.TestUtils.WORD_NOT_FOUND;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -37,6 +46,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -45,6 +55,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class WordControllerImplTest {
 
     public static final String URL = "/words";
+    public static final String DEFINITION_PATH = "/{name}/definitions";
+    public static final String WORD_NAME_PATH = "/{name}";
 
     @Autowired
     private MockMvc mockMvc;
@@ -57,6 +69,9 @@ class WordControllerImplTest {
     @BeforeEach
     void setUp() {
         WORD_DTO.setName("test");
+        WORD_DTO.getDefinitions().clear();
+        WORD_DTO.addDefinition(EXISTING_DEFINITION_DTO_FOR_WORD);
+        WORD_DTO.getExamples().clear();
     }
 
     @Test
@@ -81,7 +96,7 @@ class WordControllerImplTest {
     void testGetWordByName_whenGetWordByName_thenReturnWordDto() throws Exception {
         when(wordFacade.getWordByName(anyString())).thenReturn(WORD_DTO);
 
-        String response = mockMvc.perform(get(URL + "/{name}", WORD_DTO.getName()))
+        String response = mockMvc.perform(get(URL + WORD_NAME_PATH, WORD_DTO.getName()))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(APPLICATION_JSON))
                 .andReturn()
@@ -98,7 +113,7 @@ class WordControllerImplTest {
     void testGetWordByName_whenGetWordByInvalidName_thenThrow() throws Exception {
         when(wordFacade.getWordByName(anyString())).thenThrow(new ResourceNotFoundException(WORD_NOT_FOUND));
 
-        String response = mockMvc.perform(get(URL + "/{name}", WORD_DTO.getName()))
+        String response = mockMvc.perform(get(URL + WORD_NAME_PATH, WORD_DTO.getName()))
                 .andExpect(status().isNotFound())
                 .andExpect(content().contentType(APPLICATION_JSON))
                 .andReturn()
@@ -206,9 +221,9 @@ class WordControllerImplTest {
 
     @Test
     void testDeleteWordByName_whenWordIsFound_thenReturnNoContent() throws Exception {
-        doNothing().when(wordFacade).deleteWordByName(WORD.getName());
+        doNothing().when(wordFacade).deleteWordByName(WORD_DTO.getName());
 
-        mockMvc.perform(delete(URL + "/{name}", WORD.getName())
+        mockMvc.perform(delete(URL + WORD_NAME_PATH, WORD_DTO.getName())
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isNoContent())
                 .andReturn()
@@ -218,9 +233,9 @@ class WordControllerImplTest {
 
     @Test
     void testDeleteWordByName_whenDeleteNonExistingWord_thenThrow() throws Exception {
-        doThrow(new ResourceNotFoundException(WORD_NOT_FOUND)).when(wordFacade).deleteWordByName(WORD.getName());
+        doThrow(new ResourceNotFoundException(WORD_NOT_FOUND)).when(wordFacade).deleteWordByName(WORD_DTO.getName());
 
-        String response = mockMvc.perform(delete(URL + "/{name}", WORD.getName())
+        String response = mockMvc.perform(delete(URL + WORD_NAME_PATH, WORD_DTO.getName())
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isNotFound())
                 .andReturn()
@@ -231,5 +246,111 @@ class WordControllerImplTest {
 
         assertEquals(WORD_NOT_FOUND, actualErrorMap.get("error"));
         verify(wordFacade).deleteWordByName(anyString());
+    }
+
+    @Test
+    void testAddDefinitionToWord() throws Exception {
+        WORD_DTO.addDefinition(DEFINITION_DTO);
+
+        when(wordFacade.addDefinitionToWord(anyString(), any(DefinitionDto.class)))
+                .thenReturn(WORD_DTO);
+
+        String response = mockMvc.perform(put(URL + DEFINITION_PATH, WORD_DTO.getName())
+                        .content(mapper.writeValueAsString(DEFINITION_DTO))
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        WordDto updatedWord = mapper.readValue(response, WordDto.class);
+        assertTrue(updatedWord.getDefinitions().contains(DEFINITION_DTO));
+    }
+
+    @Test
+    void testAddDefinitionToWord_whenAddExistingDefinitionInWord_thenThrow() throws Exception {
+        WORD_DTO.addDefinition(DEFINITION_DTO);
+
+        when(wordFacade.addDefinitionToWord(anyString(), any(DefinitionDto.class)))
+                .thenThrow(new DuplicateResourceException(DEFINITION_IS_PRESENT));
+
+        String response = mockMvc.perform(put(URL + DEFINITION_PATH, WORD_DTO.getName())
+                        .content(mapper.writeValueAsString(DEFINITION_DTO))
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isConflict())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        Map<String, String> errorMap = mapper.readValue(response, HashMap.class);
+        assertEquals(DEFINITION_IS_PRESENT, errorMap.get("error"));
+    }
+
+    @Test
+    void testRemoveDefinition() throws Exception {
+        when(wordFacade.removeDefinitionFromWord(anyString(), any(DefinitionDto.class)))
+                .thenReturn(WORD_DTO);
+
+        String response = mockMvc.perform(delete(URL + DEFINITION_PATH, WORD_DTO.getName())
+                        .content(mapper.writeValueAsString(DEFINITION_DTO))
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        WordDto updatedWord = mapper.readValue(response, WordDto.class);
+        assertFalse(updatedWord.getDefinitions().contains(DEFINITION_DTO));
+    }
+
+    @Test
+    void testRemoveDefinition_whenIsTheOnlyDefinition_thenThrow() throws Exception {
+        when(wordFacade.removeDefinitionFromWord(anyString(), any(DefinitionDto.class)))
+                .thenThrow(new IllegalOperationException(ONLY_ONE_DEFINITION));
+
+        String result = mockMvc.perform(delete(URL + DEFINITION_PATH, WORD_DTO.getName())
+                        .content(mapper.writeValueAsString(DEFINITION_DTO))
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        Map<String, String> errorMap = mapper.readValue(result, HashMap.class);
+        assertEquals(ONLY_ONE_DEFINITION, errorMap.get("error"));
+    }
+
+    @Test
+    void testRemoveDefinition_whenRemoveNonExistingDefinition_thenThrow() throws Exception {
+        when(wordFacade.removeDefinitionFromWord(anyString(), any(DefinitionDto.class)))
+                .thenThrow(new ResourceNotFoundException(DEFINITION_NOT_FOUND));
+
+        String response = mockMvc.perform(delete(URL + DEFINITION_PATH, WORD_DTO.getName())
+                        .content(mapper.writeValueAsString(DEFINITION_DTO))
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        Map<String, String> errorMap = mapper.readValue(response, HashMap.class);
+        assertEquals(DEFINITION_NOT_FOUND, errorMap.get("error"));
+    }
+
+    @Test
+    void testRemoveDefinition_whenRemoveNonExistingDefinitionInWord_thenThrow() throws Exception {
+        when(wordFacade.removeDefinitionFromWord(anyString(), any(DefinitionDto.class)))
+                .thenThrow(new ResourceNotFoundException(DEFINITION_NOT_FOUND_FOR_THE_WORD));
+
+        String response = mockMvc.perform(delete(URL + DEFINITION_PATH, WORD_DTO.getName())
+                        .content(mapper.writeValueAsString(DEFINITION_DTO))
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        Map<String, String> errorMap = mapper.readValue(response, HashMap.class);
+        assertEquals(DEFINITION_NOT_FOUND_FOR_THE_WORD, errorMap.get("error"));
     }
 }
