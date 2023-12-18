b75c3b61cbd4bbbd50db260cef29e2c535b0ba09
Anisoara Vlasi <anisoara.vlasi@iongroup.com>
2023-12-18 13:35:45 +0200

    Add test for getAllCategories method

--- src/test/java/com/example/dictionary/domain/service/impl/CategoryServiceImplTest.java
+++ src/test/java/com/example/dictionary/domain/service/impl/CategoryServiceImplTest.java
@@ -8,10 +8,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.example.dictionary.utils.TestUtils.CATEGORY;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -31,5 +33,14 @@ class CategoryServiceImplTest {
         Optional<Category> categoryByName = categoryService.getCategoryByName(CATEGORY.getName());
 
         assertEquals(CATEGORY, categoryByName.get());
+    }
+
+    @Test
+    void testGetAllCategories() {
+        when(categoryRepository.findAll()).thenReturn(List.of(CATEGORY));
+
+        List<Category> categories = categoryService.getAllCategories();
+
+        assertTrue(categories.contains(CATEGORY));
     }
 }
