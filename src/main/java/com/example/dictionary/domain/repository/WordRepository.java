a5a5f732bd80f9aa2ed7ce5767ab3716e98bfc3b
Ana <anavlas1999@gmail.com>
2024-01-01 19:47:32 +0200

    Add Reporting

--- src/main/java/com/example/dictionary/domain/repository/WordRepository.java
+++ src/main/java/com/example/dictionary/domain/repository/WordRepository.java
@@ -1,5 +1,6 @@
 package com.example.dictionary.domain.repository;
 
+import com.example.dictionary.application.report.data.WordDetail;
 import com.example.dictionary.domain.entity.Word;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface WordRepository extends JpaRepository<Word, Integer> {
@@ -19,4 +21,10 @@ public interface WordRepository extends JpaRepository<Word, Integer> {
     @Transactional
     @Query("DELETE FROM Word w WHERE w.name = :name")
     void deleteByName(@Param("name") String name);
+
+    @Query("SELECT new com.example.dictionary.application.report.data.WordDetail(w.id, w.name, c.name, u, w.addedAt)" +
+            " FROM Word w" +
+            " LEFT JOIN w.contributors u " +
+            " LEFT JOIN w.category c")
+    List<WordDetail> getWordsDetails();
 }
